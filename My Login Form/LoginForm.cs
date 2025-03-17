8f8dd8a9234d91d4a22816df9e8e59bffeba08eb
weilungu <s1131375@o365st.pu.edu.tw>
2025-03-17 11:56:59 +0800

    增加 MyLogicForm

---
+++ My Login Form/LoginForm.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace My_Login_Form
+{
+    internal class LoginForm : Form
+    {
+        // LoginForm 建構子
+        public LoginForm()
+        {
+
+            InitializeForm(this);
+
+        }
+
+        public void InitializeForm(Form myForm)
+        {
+
+            Label lblMessage = new Label();
+
+            lblMessage.Text = "帳號:";
+
+            lblMessage.Location = new Point(50, 50);
+
+            myForm.Controls.Add(lblMessage);
+
+            //
+
+            TextBox tbAccount = new TextBox();
+
+            tbAccount.Width = 150;
+
+            tbAccount.Location = new Point(80, 50);
+
+            myForm.Controls.Add(tbAccount);
+
+            //
+
+            Button btnOK = new Button();
+
+            btnOK.Text = "OK";
+
+            btnOK.Width = 100;
+
+            btnOK.Location = new Point(50, 100);
+
+            myForm.Controls.Add(btnOK);
+
+            //
+
+            btnOK.Click += BtnOK_Click;
+
+        }
+
+        private void BtnOK_Click(object sender, EventArgs e)
+        {
+
+            System.Console.WriteLine("OK clicked!");
+
+        }
+    }
+}
---
+++ My Login Form/Program.cs
@@ -0,0 +1,20 @@
+﻿using System.Drawing;
+using System.Windows.Forms;
+
+namespace My_Login_Form
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Form myForm = new LoginForm();
+
+            myForm.Size = new Size(320, 240);
+
+            myForm.Text = "S1131375's Login Form";  // S1135566 請改為自己的學號！
+            //
+
+            Application.Run(myForm);
+        }
+    }
+}
