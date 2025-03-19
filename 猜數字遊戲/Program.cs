cf8926767d4410aeb6b5473af517939c1e2894e5
weilungu <s1131375@o365st.pu.edu.tw>
2025-03-19 18:13:26 +0800

    更新"猜數字遊戲"規則

--- 猜數字遊戲/Program.cs
+++ 猜數字遊戲/Program.cs
@@ -6,25 +6,40 @@ namespace S1131375XAXBGame
     {
         static void Main(string[] args)
         {
-            // 1. 生成猜數字遊戲核心模組
-            XAXBKernel xaxb = new XAXBKernel();
-            // 2. 手動設定幸運號碼
-            xaxb.SetLuckyNumber("1234");
-            // 3-1. 驗證相關模組是否設計正確？
-            Console.WriteLine("{0} is legal? {1}", "1234", XAXBKernel.IsLegal("1234"));
-            Console.WriteLine("{0} is legal? {1}", "123", XAXBKernel.IsLegal("123"));
-            Console.WriteLine("{0}: {1}", "5665", XAXBKernel.IsLegal("5665") ? "legal" : "illegal");
-            Console.WriteLine("{0}: {1}", "7878", XAXBKernel.IsLegal("7878") ? "legal" : "illegal");
-            Console.WriteLine("{0}: {1}", "7897", XAXBKernel.IsLegal("7897") ? "legal" : "illegal");
-            Console.WriteLine("{0}: {1}", "0789", XAXBKernel.IsLegal("0789") ? "legal" : "illegal");
-            // 3-2. 驗證相關模組是否設計正確？
-            String result1 = xaxb.GetGuessResult("1245");
-            Console.WriteLine("{0} {1}: {2}", xaxb.GetLuckyNumber(), "1245", result1);
-            String result2 = xaxb.GetGuessResult("1234");
-            Console.WriteLine("{0} {1}: {2}", xaxb.GetLuckyNumber(), "1234", result2);
-            // 3-3. 驗證相關模組是否設計正確？
-            Console.WriteLine("{0} gameover: {1}", "1254", xaxb.IsGameover("1254"));
-            Console.WriteLine("{0} gameover: {1}", "1234", xaxb.IsGameover("1234"));
+            string playerInput;
+
+            XAXBKernel kernel = new XAXBKernel();
+            kernel.SetLuckyNumber(Console.ReadLine()); // 設定幸運數字
+            Console.WriteLine($"Lucky Number: {kernel.GetLuckyNumber()}");
+            do
+            {
+                string guessResult;
+
+                // 過濾掉不合法的輸入，直到該輸入合法為止
+                bool illegal;
+                do
+                {
+                    playerInput = Console.ReadLine();
+                    illegal = !XAXBKernel.IsLegal(playerInput);
+
+                    if(playerInput == "")
+                    {
+                        Console.WriteLine("");
+                    }
+                    else if (illegal)
+                    {
+                        Console.WriteLine($"{playerInput}: illegal");
+                    }
+
+                } while (illegal);
+
+                // 進行比對
+                guessResult = kernel.GetGuessResult(playerInput);
+                Console.WriteLine($"{playerInput}: {guessResult}");
+
+            } while (!kernel.IsGameover(playerInput)); // 遊戲結束代表玩家贏了
+
+            Console.WriteLine("You rock!");
         }
     }
 }
