1fb8881492df8ac020095c4cb3073150e7021a42
weilungu <s1131375@o365st.pu.edu.tw>
2025-03-29 10:44:38 +0800

    增加"踩地雷"

---
+++ 踩地雷/Map.cs
@@ -0,0 +1,68 @@
+﻿using System;
+
+namespace 踩地雷
+{
+    internal class Map
+    {
+        int size;
+        char emptyElement, landminesElement;
+        public Map(int size)
+        {
+            this.size = size;
+            this.emptyElement = '_';
+            this.landminesElement = '*';
+        }
+
+        public string Creat()
+        {
+            string map = "";
+            for(int i = 0; i < size; i++)
+            {
+                for(int j = 0; j < size; j++)
+                {
+                    map += emptyElement;
+                }
+                if(i == size - 1) { break; }
+                map += '\n';
+            }
+            return map;
+        }
+        public string SetLandmines(string map, int input_i, int input_j)
+        {
+            string[] newMap = map.Split('\n');
+            char[,] coordinate = new char[size, size];
+            for (int i=0; i<size; i++)
+            {
+                for(int j=0; j<size; j++)
+                {
+                    coordinate[i, j] = newMap[i].ToCharArray()[j];
+                }
+            }
+
+            for (int i = 0; i < size; i++)
+            {
+                for(int j = 0; j < size; j++)
+                {
+                    bool Delivery = i == input_i-1 && j == input_j-1; // 是否要投放地雷
+                    if( Delivery)
+                    {
+                        coordinate[i, j] = landminesElement;
+                    }
+                }
+            }
+
+            // 合併
+            string result = "";
+            for(int i = 0; i < size; i++)
+            {
+                for(int j = 0; j < size; j++)
+                {
+                    result += coordinate[i, j];
+                }
+                result += "\n";
+            }
+
+            return result;
+        }
+    }
+}
---
+++ 踩地雷/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace 踩地雷
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] data = Console.ReadLine().Split(' ');
+            int size = int.Parse(data[0]);
+            int landminesNum = int.Parse(data[1]);
+            int[,] coordinate = new int[landminesNum, 2];
+
+            for (int i = 0; i < landminesNum; i++)
+            {
+                string[] storage = Console.ReadLine().Split(' ');
+                coordinate[i, 0] = int.Parse(storage[0]);
+                coordinate[i, 1] = int.Parse(storage[1]);
+            }
+
+            Map m = new Map(size);
+            string map = m.Creat();
+
+            for (int i = 0; i < landminesNum; i++)
+            {
+                map = m.SetLandmines(map, coordinate[i, 0], coordinate[i, 1]);
+            }
+            Console.WriteLine(map);
+        }
+    }
+}
