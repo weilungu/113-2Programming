757c8dc75b34809d2c637b7b09a243b565540ef7
weilungu <s1131375@o365st.pu.edu.tw>
2025-05-17 12:08:39 +0800

    增加計時器程式

---
+++ 計時器/Form1.Designer.cs
@@ -0,0 +1,83 @@
+﻿namespace 計時器
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            components = new System.ComponentModel.Container();
+            lb_displayTime = new Label();
+            btn_start = new Button();
+            timer1 = new System.Windows.Forms.Timer(components);
+            SuspendLayout();
+            // 
+            // lb_displayTime
+            // 
+            lb_displayTime.AutoSize = true;
+            lb_displayTime.BackColor = SystemColors.ActiveBorder;
+            lb_displayTime.Font = new Font("Microsoft JhengHei UI", 48F, FontStyle.Regular, GraphicsUnit.Point, 136);
+            lb_displayTime.Location = new Point(113, 46);
+            lb_displayTime.Name = "lb_displayTime";
+            lb_displayTime.Size = new Size(181, 102);
+            lb_displayTime.TabIndex = 0;
+            lb_displayTime.Text = "100";
+            // 
+            // btn_start
+            // 
+            btn_start.Font = new Font("Microsoft JhengHei UI", 18F, FontStyle.Bold, GraphicsUnit.Point, 136);
+            btn_start.ForeColor = SystemColors.MenuHighlight;
+            btn_start.Location = new Point(131, 277);
+            btn_start.Name = "btn_start";
+            btn_start.Size = new Size(149, 83);
+            btn_start.TabIndex = 1;
+            btn_start.Text = "Start";
+            btn_start.UseVisualStyleBackColor = true;
+            btn_start.Click += btn_start_Click;
+            // 
+            // timer1
+            // 
+            timer1.Tick += timer1_Tick;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(9F, 19F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(424, 450);
+            Controls.Add(btn_start);
+            Controls.Add(lb_displayTime);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label lb_displayTime;
+        private Button btn_start;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
---
+++ 計時器/Form1.cs
@@ -0,0 +1,33 @@
+namespace 計時器
+{
+    public partial class Form1 : Form
+    {
+        int counter;
+        public Form1()
+        {
+            InitializeComponent();
+
+            counter = 100; // 設定計時時間
+
+            timer1.Interval = 1000; // 時間間隔為 1000 毫秒 = 1 秒
+        }
+
+        private void btn_start_Click(object sender, EventArgs e) // 當按了開始按鈕後
+        {
+            timer1.Start(); // 開始計時
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            counter--;
+            lb_displayTime.Text = counter.ToString();
+
+            if(counter <= 0) // 當計時器的值 <= 0 時
+            {
+                timer1.Stop(); // 停止計時
+                counter = 100;
+                lb_displayTime.Text = counter.ToString(); // 重新校正顯示時間的標籤
+            }
+        }
+    }
+}
