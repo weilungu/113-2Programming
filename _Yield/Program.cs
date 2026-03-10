cbbea266c50ecd342c6722937ec2b752184d5791
weilungu <s1131375@o365st.pu.edu.tw>
2026-03-10 18:24:55 +0800

    new files

---
+++ _Yield/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace Yield;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        MyMath m =  new MyMath();
+
+        int fib = m.Fibonacci(10);
+
+        Console.WriteLine(fib);
+    }
+}
